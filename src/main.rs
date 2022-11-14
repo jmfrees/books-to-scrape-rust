ed57b6e9f13636be37883e4b8e2920e41118c3fa
Jonathan Frees <jmfrees@uab.edu>
2022-11-14 17:41:02 -0600

    Add logging for observability

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use futures::{stream, StreamExt};
 use std::num::ParseIntError;
 
+use env_logger::Env;
 use eyre::{eyre, Result};
+use log;
 use url::{ParseError, Url};
 
 #[derive(Debug)]
@@ -48,7 +50,10 @@ impl Book {
         let sel = make_selector("div[class$='product_main'] h1");
         match book_page.select(&sel).next() {
             Some(elem) => Ok(elem.text().collect()),
-            None => Err(eyre!("Failed to extract title from book page")),
+            None => {
+                log::warn!("Failed to extract title from book page");
+                Err(eyre!("Failed to extract title from book page"))
+            }
         }
     }
 
@@ -56,7 +61,10 @@ impl Book {
         let sel = make_selector("tbody tr:first-of-type td");
         match book_page.select(&sel).next() {
             Some(elem) => Ok(elem.text().collect()),
-            None => Err(eyre!("Failed to extract upc from book page")),
+            None => {
+                log::warn!("Failed to extract upc from book page");
+                Err(eyre!("Failed to extract upc from book page"))
+            }
         }
     }
 
@@ -64,7 +72,10 @@ impl Book {
         let sel = make_selector("div[class$='product_main']  p[class^='price']");
         match book_page.select(&sel).next() {
             Some(elem) => Ok(elem.text().collect()),
-            None => Err(eyre!("Failed to extract price from book page")),
+            None => {
+                log::warn!("Failed to extract price from book page");
+                Err(eyre!("Failed to extract price from book page"))
+            }
         }
     }
 
@@ -72,7 +83,10 @@ impl Book {
         let sel = make_selector("div[class$='product_main'] p[class^='instock']");
         let text: String = match book_page.select(&sel).next() {
             Some(elem) => elem.text().collect(),
-            None => return Err(eyre!("Failed to extract availability from book page")),
+            None => {
+                log::warn!("Failed to availability from book page");
+                return Err(eyre!("Failed to availability from book page"));
+            }
         };
 
         Ok(parse_int(&text).unwrap_or(0))
@@ -113,6 +127,7 @@ fn make_selector(selector: &str) -> scraper::Selector {
 }
 
 fn parse_int(input: &str) -> Result<u32, ParseIntError> {
+    log::debug!("Attempting to parse input {}", input);
     input
         .chars()
         .skip_while(|c| !c.is_ascii_digit())
@@ -122,8 +137,11 @@ fn parse_int(input: &str) -> Result<u32, ParseIntError> {
 }
 
 async fn get_html(url: Url) -> Result<scraper::Html> {
-    let resp = reqwest::get(url).await?.text().await?;
-    Ok(scraper::Html::parse_document(&resp))
+    log::debug!("Requesting resource at url {}", url);
+    let resp = reqwest::get(url).await?;
+    log::debug!("Parsing result from url: {}", resp.url());
+    let resp_text = resp.text().await?;
+    Ok(scraper::Html::parse_document(&resp_text))
 }
 
 fn get_category_paths(page: &scraper::Html) -> Vec<&str> {
@@ -138,14 +156,15 @@ fn get_category_paths(page: &scraper::Html) -> Vec<&str> {
 }
 
 fn build_book_page_url(path: &str) -> Result<Url, ParseError> {
+    log::trace!("Building book page url with path: {}", path);
     build_books_toscrape_url("catalogue/")?.join(path.trim_start_matches("../"))
 }
 
 fn build_books_toscrape_url(path: &str) -> Result<Url, ParseError> {
+    log::trace!("Building url with path: {}", path);
     const HOMEPAGE: &str = "https://books.toscrape.com/";
     Url::parse(HOMEPAGE)?.join(path)
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -210,6 +229,8 @@ mod tests {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let book_url_selector = make_selector("article.product_pod a[title]");
 
     let document = get_html(build_books_toscrape_url("").expect("Getting base url must be valid"))
@@ -217,13 +238,15 @@ async fn main() -> Result<()> {
         .unwrap_or_else(|error| panic!("Problem requesting home page {:?}", error));
 
     let category_paths = get_category_paths(&document);
+    log::debug!("Category Paths: {category_paths:?}");
     let category_urls = category_paths
         .iter()
         .filter_map(|x| build_books_toscrape_url(x).ok());
+    log::debug!("Category urls: {category_urls:?}");
 
     let pages: Vec<scraper::Html> = stream::iter(category_urls)
         .filter_map(|url| async {
-            println!("Getting category page {url}");
+            log::info!("Getting category page {url}");
             get_html(url).await.ok()
         })
         .collect()
@@ -237,7 +260,7 @@ async fn main() -> Result<()> {
 
     let books = stream::iter(book_urls)
         .filter_map(|url| async {
-            println!("Getting book page at {url}");
+            log::info!("Getting book page at {url}");
             get_html(url).await.ok()
         })
         .map(|page| Book::from_html(&page))
@@ -245,9 +268,10 @@ async fn main() -> Result<()> {
         .await;
     for book in &books {
         match book {
-            Ok(book) => println!("{book:?}"),
+            Ok(book) => log::info!("{book:?}"),
             Err(_) => (),
         }
     }
+    log::info!("Number of books scraped: {}", books.len());
     Ok(())
 }
